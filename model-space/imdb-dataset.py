221c997d6f0e44f69ae3f713a2c0d29c3565622f
Lucas Rocha <lr359508@gmail.com>
2024-04-13 21:02:53 -0300

    feat: subplots added

--- model-space/imdb-dataset.py
+++ model-space/imdb-dataset.py
@@ -1,7 +1,7 @@
 
 import tensorflow as tf
 from tensorflow import keras
-import numpy as np
+import matplotlib.pyplot as plt
 
 # Download
 imdb = keras.datasets.imdb
@@ -72,3 +72,29 @@ history = model.fit(partial_x_train,
 # Model evaluating
 results = model.evaluate(test_data,  test_labels, verbose=2)
 print(results)
+
+
+# Matplotlib
+history_dict = history.history
+
+acc = history_dict['accuracy']
+val_acc = history_dict['val_accuracy']
+loss = history_dict['loss']
+val_loss = history_dict['val_loss']
+
+epochs = range(1, len(acc) + 1)
+
+plt.plot(epochs, loss, 'bo', label='Training loss')
+plt.plot(epochs, val_loss, 'b', label='Validation loss')
+plt.title('Training and validation loss')
+plt.xlabel('Epochs')
+plt.ylabel('Loss')
+
+plt.plot(epochs, acc, 'bo', label='Training acc')
+plt.plot(epochs, val_acc, 'b', label='Validation acc')
+plt.title('Training and validation accuracy')
+plt.xlabel('Epochs')
+plt.ylabel('Accuracy')
+
+plt.legend()
+plt.show()
