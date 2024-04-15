07a242ee853bd8f59f2857ae0885896d2aaf8c86
Lucas Rocha <lr359508@gmail.com>
2024-04-14 22:46:32 -0300

    feat: CSS ajustment

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,3 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 import { ReviewForm } from './components/ReviewForm'
 
@@ -8,7 +5,9 @@ function App() {
 
   return (
     <>
-      <ReviewForm />
+      <main>
+        <ReviewForm />
+      </main>
     </>
   )
 }
--- frontend/src/components/ReviewForm.tsx
+++ frontend/src/components/ReviewForm.tsx
@@ -2,14 +2,12 @@ import { useState } from "react";
 
 interface ReviewFormData {
     title: string;
-    rating: number;
     review: string;
 }
 
 export function ReviewForm(){
     const [formData, setFormData] = useState<ReviewFormData>({
         title: '',
-        rating: 0,
         review: '',
     })
     
@@ -18,48 +16,43 @@ export function ReviewForm(){
 
         setFormData({
         title: '',
-        rating: 0,
         review: '',
         })
     }
 
     return(
-        <form onSubmit={handleSubmit}>
-        <div>
-            <label htmlFor="title">Title:</label>
-            <input
-            type="text"
-            id="title"
-            name="title"
-            value={formData.title}
-            onChange={e => setFormData({...formData, title : e.target.value})}
-            required
-            />
-        </div>
-        <div>
-        <label htmlFor="rating">Rating:</label>
-        <input
-            type="number"
-            id="rating"
-            name="rating"
-            min="1"
-            max="5"
-            value={formData.rating}
-            onChange={e => setFormData({...formData, rating : parseInt(e.target.value)})}
-            required
-        />
-      </div>
-        <div>
-            <label htmlFor="review">Review:</label>
-            <textarea
-            id="review"
-            name="review"
-            value={formData.review}
-            onChange={e => setFormData({...formData, review : e.target.value})}
-            required
-            />
-        </div>
-        <button type="submit">Submit</button>
-        </form>
+        <>
+            <h1>Your review</h1>
+
+            <form onSubmit={handleSubmit}>
+                <div>
+                    <label htmlFor="title">Title:</label>
+                    <input
+                    type="text"
+                    id="title"
+                    name="title"
+                    value={formData.title}
+                    onChange={e => setFormData({...formData, title : e.target.value})}
+                    required
+                    />
+                </div>
+                <div>
+                    <label htmlFor="review">Review:</label>
+                    <textarea
+                    id="review"
+                    name="review"
+                    rows={5}
+                    cols={25}
+                    value={formData.review}
+                    onChange={e => setFormData({...formData, review : e.target.value})}
+                    required
+                    />
+                </div>
+                <div>
+                    <button type="reset">Reset</button>
+                    <button type="submit">Submit</button>
+                </div>
+            </form>
+        </>
     )
 }
