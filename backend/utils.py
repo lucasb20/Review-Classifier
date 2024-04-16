105ae96baf856c0c0801c7423082fa42c152dd84
Lucas Rocha <lr359508@gmail.com>
2024-04-16 15:37:22 -0300

    fix: dims ajustment

--- backend/utils.py
+++ backend/utils.py
@@ -6,7 +6,6 @@ import json
 
 def predict(data):
     model = keras.models.load_model("static/modelv1.keras")
-    data = np.expand_dims(data, axis=0)
     return model.predict(data)[0][0] > 0.5
 
 def preprocess_text(text : str):
@@ -14,10 +13,11 @@ def preprocess_text(text : str):
     words = text.lower().split()
 
     with open("static/word_index.json",'r') as json_file:
-        word_index : dict = json.dump(json_file)
+        word_index = json.load(json_file)
 
     word_indices = [word_index.get(word, word_index["<UNK>"]) for word in words]
-    
+    word_indices = np.expand_dims(word_indices, axis=0)
+
     pad_sequence = keras.utils.pad_sequences(word_indices, maxlen=256, padding='post', value=word_index["<PAD>"])
 
     return pad_sequence
