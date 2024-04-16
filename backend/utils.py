13a201a36b6b8deba174cdcab52578ca29603916
Lucas Rocha <lr359508@gmail.com>
2024-04-16 16:52:06 -0300

    feat: word_index start append

--- backend/utils.py
+++ backend/utils.py
@@ -22,6 +22,7 @@ def preprocess_text(text : str):
         word_index = json.load(json_file)
 
     word_indices = [word_index.get(word, word_index["<UNK>"]) for word in words]
+    word_indices.insert(0, word_index["<START>"])
     word_indices = np.expand_dims(word_indices, axis=0)
 
     pad_sequence = keras.utils.pad_sequences(word_indices, maxlen=256, padding='post', value=word_index["<PAD>"])
