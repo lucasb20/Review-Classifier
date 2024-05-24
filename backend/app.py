d144f6086160e034e474c18179f696ff65ee725a
Lucas Rocha <lr359508@gmail.com>
2024-05-24 08:42:13 -0300

    feat: flask smorest removed

--- backend/app.py
+++ backend/app.py
@@ -1,16 +1,12 @@
 from flask import Flask
-from flask_smorest import Api
 from flask_cors import CORS
 from resources import blp
 
 import os
 
 app = Flask(__name__)
-app.config["API_TITLE"] = "My API"
-app.config["API_VERSION"] = "v1"
-app.config["OPENAPI_VERSION"] = "3.0.2"
-frontend_host = os.getenv("frontend_host", "http://localhost:5000")
-CORS(app, origins=[frontend_host])
-api = Api(app)
+app.config['SECRET_KEY'] = os.getenv("SECRET_KEY") or 'dev'
 
-api.register_blueprint(blp)
+CORS(app)
+
+app.register_blueprint(blp)
