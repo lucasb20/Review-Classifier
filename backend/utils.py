b024f81ada10a3deb24703dd1f513c26578013a5
Lucas Rocha <lr359508@gmail.com>
2024-05-24 11:17:14 -0300

    feat: pad_sequence created

--- backend/utils.py
+++ backend/utils.py
@@ -11,16 +11,22 @@ def createInterpreter():
     interpreter.resize_tensor_input(input_details[0]['index'], (1, 256))
     interpreter.resize_tensor_input(output_details[0]['index'], (1, 1))
     interpreter.allocate_tensors()
-    return interpreter, input_details[0]['index'], output_details[0]['index']
+    return interpreter, (input_details[0]['index'], output_details[0]['index'])
 
 interpreter, (input_index, output_index) = createInterpreter()
 
 def predict(data):
-    input_data = np.array(data, np.float32)
+    input_data = np.array([data], np.float32)
     interpreter.set_tensor(input_index, input_data)
     interpreter.invoke()
     output_data = interpreter.get_tensor(output_index)
-    return 1 if output_data(data)[0][0] > 0.60 else 0
+    return 1 if output_data[0][0] > 0.60 else 0
+
+def pad_sequence(text, maxlen, value):
+    x = np.full((maxlen,), value, dtype=np.float32)
+    trunc = text[:maxlen]
+    x[0: len(trunc)] = trunc
+    return x
 
 def preprocess_text(text : str):
     text = re.sub(r'[^\w\s]', '', text)
@@ -29,10 +35,7 @@ def preprocess_text(text : str):
     with open("static/word_index.json",'r') as json_file:
         word_index = json.load(json_file)
 
-    word_indices = [word_index.get(word, word_index["<UNK>"]) for word in words]
+    word_indices = [ word_index.get(word, word_index["<UNK>"]) for word in words ]
     word_indices.insert(0, word_index["<START>"])
-    word_indices = np.expand_dims(word_indices, axis=0)
 
-    pad_sequence = keras.utils.pad_sequences(word_indices, maxlen=256, padding='post', value=word_index["<PAD>"])
-
-    return pad_sequence
+    return pad_sequence(word_indices, maxlen=256, value=word_index["<PAD>"])
