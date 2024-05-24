65f3a2466809f5ce12ea37318f05f13e1bb7d283
Lucas Rocha <lr359508@gmail.com>
2024-05-24 11:36:46 -0300

    ref: nextjs changed to nodejs

--- frontend/app/page.tsx
+++
@@ -1,9 +0,0 @@
-import { ReviewForm } from "@/components/ReviewForm";
-
-export default function Home() {
-  return (
-      <main className="main" >
-        <ReviewForm />
-      </main>
-    )
-}
