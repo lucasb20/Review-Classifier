a92ab0870b9f837e5d0894996a3375b256428b8a
Lucas Rocha <lr359508@gmail.com>
2024-05-24 10:10:21 -0300

    feat: createInterpreter created and predict implemented

--- backend/utils.py
+++ backend/utils.py
@@ -4,19 +4,23 @@ import numpy as np
 import re
 import json
 
-def createModel():
+def createInterpreter():
     interpreter = tflite.Interpreter(model_path="static/my_model.tflite")
     input_details = interpreter.get_input_details()
     output_details = interpreter.get_output_details()
     interpreter.resize_tensor_input(input_details[0]['index'], (1, 256))
     interpreter.resize_tensor_input(output_details[0]['index'], (1, 1))
     interpreter.allocate_tensors()
-    return model
+    return interpreter, input_details[0]['index'], output_details[0]['index']
 
-model = createModel()
+interpreter, (input_index, output_index) = createInterpreter()
 
-def predict(data):    
-    return 1 if model.predict(data)[0][0] > 0.60 else 0
+def predict(data):
+    input_data = np.array(data, np.float32)
+    interpreter.set_tensor(input_index, input_data)
+    interpreter.invoke()
+    output_data = interpreter.get_tensor(output_index)
+    return 1 if output_data(data)[0][0] > 0.60 else 0
 
 def preprocess_text(text : str):
     text = re.sub(r'[^\w\s]', '', text)
