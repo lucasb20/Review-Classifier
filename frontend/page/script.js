c979f440ea6f515724adc50cc7a790808519347a
Lucas Rocha <lr359508@gmail.com>
2024-05-25 16:09:16 -0300

    feat: api service implemented

--- frontend/page/script.js
+++ frontend/page/script.js
@@ -1,20 +1,34 @@
 
 const resContainer = document.getElementsByTagName('p')[0]
 
-function handleSubmit(event){
-    event.preventDefault();
+document.getElementsByTagName('form')[0].addEventListener('submit', handleSubmit)
+
+function handleSubmit(ev){
+    ev.preventDefault();
     
-    postReview(formData.review)
+    postReview({
+        title: ev.target['title'].value,
+        review: ev.target['review'].value
+    })
     .then(data => data.json())
     .then(data => {
         const predictBool = data.predict
-        resContainer.style.color = predictBool === 1 ? 'green' : 'red'
-        resContainer.innerText = predictBool === 1 ? "That's great you enjoyed it." : "It's unfortunate you didn't enjoy it."
+        resContainer.style.color = predictBool ? 'green' : 'red'
+        resContainer.innerText = predictBool ? "That's great you enjoyed it." : "It's unfortunate you didn't enjoy it."
     })
     .catch(err => alert("Error connecting to the API."))
 }
 
-document.getElementsByTagName('form')[0].addEventListener('submit', ev => {
-    ev.preventDefault()
-    console.log(ev)
-})
+async function postReview(text){
+    const API_URL = process.env.API_URL + '/reviews/'
+
+    const response = await fetch(API_URL, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ text: text })
+    })
+
+    return response
+}
