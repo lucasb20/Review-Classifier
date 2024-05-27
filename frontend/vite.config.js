018243db8c4508421540455e187261a4a2c67230
Lucas Rocha <lr359508@gmail.com>
2024-05-27 13:45:30 -0300

    feat: server configs defined

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,15 +2,11 @@ import { defineConfig, loadEnv } from 'vite'
 
 export default defineConfig(({ command, mode }) => {
   const env = loadEnv(mode, process.cwd(), '')
-  console.log(env.API_URL)
   return {
     root: './src',
     base: '/',
     build: {
       outDir: 'dist',
-    },
-    server: {
-      port: 3000,
     },
     define: {
       API_URL: JSON.stringify(env.API_URL),
